31c23d7af62795a73acca5623b83e2c4d0b44837
jbdonin <jean-baptiste.38@hotmail.com>
2021-05-16 16:40:55 +0200

    debut de seed

---
+++ prisma/seed.js
@@ -0,0 +1,81 @@
+const { PrismaClient } = require("@prisma/client")
+var faker = require('faker');
+faker.locale = "fr";
+const prisma = new PrismaClient()
+
+const main = async () => {
+    // await prisma.raisons_inscriptions.create({
+    //   data : {
+    //     libelle : 'selon feeling'
+    //   }
+    // })
+    // await prisma.raisons_inscriptions.create({
+    //   data : {
+    //     libelle : 'amitié'
+    //   }
+    // })
+    // await prisma.raisons_inscriptions.create({
+    //   data : {
+    //     libelle : 'soirée sans lendemain'
+    //   }
+    // })
+
+  for (let index = 0; index < 3; index++) {
+    await prisma.utilisateurs.create({
+      data: {
+        prenom : faker.name.firstName(),
+        email : faker.internet.email(),
+        mot_de_passe : faker.internet.password(),
+        date_de_naissance :faker.date.between('1997-01-01', '2015-01-05'),
+        biographie : faker.lorem.text(),
+        lri:{
+          create : {
+            raison_inscription :{
+              connect : {
+                id:2
+              }
+            }
+          }
+        },
+        photo_utilisateur : {
+          create : [
+          {
+          photo_url : faker.image.imageUrl(),
+          est_photo_profil : true,
+          },
+          {
+          photo_url : faker.image.imageUrl(),
+          est_photo_profil : true,
+          }
+        ]
+        }
+      }
+    })
+
+  }
+  for (let index = 0; index < 3; index++) {
+    //ajouter une conversation par rapport au utilisateur créer
+    await prisma.conversations.create({
+      data : {
+        est_enregistre : true,
+        ajout_utilisateurs1:true,
+        ajout_utilisateurs2:true,
+        utilisateurs1 :{
+      connect : {
+        id : Math.floor(Math.random() * 5)+1
+      }
+      },
+      utilisateur2 : { connect :{
+        id: Math.floor(Math.random() * 5) + 1
+      }
+      }
+      }
+    })
+  }
+}
+
+main()
+  .catch(e => console.error(e))
+  .finally(async () => {
+    await prisma.$disconnect()
+  })
---
+++ prisma/seed/utilisateurs.js
@@ -0,0 +1,10 @@
+var faker = require('faker');
+faker.locale = "fr";
+
+ const utilisateurs = {
+  prenom : faker.name.firstName(),
+  email : faker.internet.email(),
+  mot_de_passe : faker.internet.password(),
+  date_de_naissance :faker.date.between('1997-01-01', '2015-01-05'),
+  biographie : faker.lorem.text()
+}
