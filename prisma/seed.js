f07b73ccc9a73a68f3b285f3da45ceef3b36b853
jbdonin <jean-baptiste.38@hotmail.com>
2021-05-21 19:11:03 +0200

    modification de message

--- prisma/seed.js
+++ prisma/seed.js
@@ -20,7 +20,7 @@ const main = async () => {
       }
     })
 
-  for (let index = 0; index < 5; index++) {
+  for (let index = 0; index <= 5; index++) {
     await prisma.utilisateurs.create({
       data: {
         prenom : faker.name.firstName(),
@@ -52,8 +52,8 @@ const main = async () => {
       }
     })
   }
-  for (let index = 0; index < 3; index++) {
-    //ajouter une conversation par rapport au utilisateur créer
+  //ajouter une conversation par rapport au utilisateur créer
+  for (let index = 0; index <= 3; index++) {
     await prisma.conversations.create({
       data : {
         est_enregistre : true,
@@ -64,12 +64,29 @@ const main = async () => {
         id : Math.floor(Math.random() * 5)+1
       }
       },
-      utilisateur2 : { connect :{
+      utilisateur2 : {
+        connect :{
         id: Math.floor(Math.random() * 5) + 1
       }
       }
       }
     })
+  }
+  //creation messages selon conversations
+  for (let index = 0; index <= 10; index++) {
+    await prisma.messages.create({
+      data : {
+        conversation : {
+          connect : {
+            id : Math.floor(Math.random() * 3) + 1
+          }
+        },
+        contenu : faker.lorem.paragraph(),
+        send_by_user1 :  Boolean(Math.round(Math.random()))
+
+      }
+    })
+
   }
 }
 
