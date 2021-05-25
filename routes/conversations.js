24ff37f3b7bb11a2d69a810e2843595a07110f1b
Mathis972 <eyzex@hotmail.com>
2021-05-25 16:23:21 +0200

    ajout recherche id pour conversations et messages

--- routes/conversations.js
+++ routes/conversations.js
@@ -1,11 +1,30 @@
-const express=require('express')
-const router=express.Router()
-const { PrismaClient } = require('@prisma/client')
+const express = require('express')
+const router = express.Router()
+const { PrismaClient, Prisma } = require('@prisma/client')
 const prisma = new PrismaClient()
 
 router.get('/', async (req, res) => {
-  const users = await prisma.conversations.findMany()
-  res.json(users)
+  if (req.query.user_id) {
+    const user_id = parseInt(req.query.user_id)
+    const conversations = await prisma.conversations.findMany({
+      include: {
+        utilisateurs1: true, utilisateur2: true
+      },
+      where: {
+        OR: [
+          {
+            fk_utilisateur2_id: user_id
+          },
+          {
+            fk_utilisateur1_id: user_id
+          }
+        ]
+      }
+    })
+    return res.json(conversations)
+  }
+  const conversations = await prisma.conversations.findMany()
+  res.json(conversations)
 })
 
-module.exports=router
+module.exports = router
