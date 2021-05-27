d440b64565bcf4fa4a86e823c348f916ea1f71aa
Mathis972 <eyzex@hotmail.com>
2021-05-27 09:21:16 +0200

    ajout delete photos selon id utilisateur

--- routes/photos_utilisateurs.js
+++ routes/photos_utilisateurs.js
@@ -1,5 +1,5 @@
-const express=require('express')
-const router=express.Router()
+const express = require('express')
+const router = express.Router()
 const { PrismaClient } = require('@prisma/client')
 const prisma = new PrismaClient()
 
@@ -8,4 +8,16 @@ router.get('/', async (req, res) => {
   res.json(users)
 })
 
-module.exports=router
+router.delete('/:user_id', async (req, res) => {
+  const { user_id } = req.params
+
+  const photos_utilisateurs = await prisma.photos_utilisateurs.deleteMany({
+    where: {
+      fk_utilisateur_id: parseInt(user_id)
+    }
+  })
+  console.log(photos_utilisateurs)
+  res.json(`les photos de l'utilisateur ${user_id} sont supprimées`)
+})
+
+module.exports = router
