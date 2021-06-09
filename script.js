0f0bef6536a13b590bdeb8425d9bd87b877a51dc
Mathis972 <eyzex@hotmail.com>
2021-06-09 20:50:28 +0200

    fix heroku

--- script.js
+++ script.js
@@ -2,35 +2,35 @@ const express = require("express");
 var bodyParser = require('body-parser')
 const cors = require('cors');
 const app = express()
-var server = app.listen(8000);
-const io = require('socket.io')(server,{
+const port = process.env.PORT || 8000;
+var server = app.listen(port);
+const io = require('socket.io')(server, {
   allowEIO3: true,
   cors: {
     origin: '*',
-  }})
+  }
+})
 app.use(express.json())
 app.use(cors());
 
 
-const port = process.env.PORT || 8000;
+app.use("/utilisateurs", require('./routes/utilisateurs'))
+app.use("/conversations", require('./routes/conversations'))
+app.use("/messages", require('./routes/messages'))
+app.use("/questions", require('./routes/questions'))
+app.use("/reponses", require('./routes/reponses'))
+app.use("/reponses_utilisateurs", require('./routes/reponses_utilisateurs'))
+app.use("/raisons_inscriptions", require('./routes/raisons_inscriptions'))
+app.use("/photos_utilisateurs", require('./routes/photos_utilisateurs'))
+app.use("/liaisons_raisons_inscriptions", require('./routes/liaisons_raisons_inscriptions'))
 
-app.use("/utilisateurs",require('./routes/utilisateurs'))
-app.use("/conversations",require('./routes/conversations'))
-app.use("/messages",require('./routes/messages'))
-app.use("/questions",require('./routes/questions'))
-app.use("/reponses",require('./routes/reponses'))
-app.use("/reponses_utilisateurs",require('./routes/reponses_utilisateurs'))
-app.use("/raisons_inscriptions",require('./routes/raisons_inscriptions'))
-app.use("/photos_utilisateurs",require('./routes/photos_utilisateurs'))
-app.use("/liaisons_raisons_inscriptions",require('./routes/liaisons_raisons_inscriptions'))
-
- const arrayRoom = 'room3'
- let id_user = ''
-io.on('connection',  (socket) =>{
+const arrayRoom = 'room3'
+let id_user = ''
+io.on('connection', (socket) => {
   let room = arrayRoom
 
   //console.log(socket.rooms);
-    socket.on('idLaunch',function (msg) {
+  socket.on('idLaunch', function (msg) {
     id_user = msg
     console.log(id_user)
     socket.join(room);
@@ -38,7 +38,7 @@ io.on('connection',  (socket) =>{
     const clients = io.sockets.adapter.rooms.get(room).size;
 
     console.log(clients2)
-    if (clients === 1){
+    if (clients === 1) {
       socket.join(room);
     } else if (clients === 2) {
       socket.join(room);
@@ -47,23 +47,23 @@ io.on('connection',  (socket) =>{
       socket.disconnect()
       console.log(clients2)
     }
-      console.log(`Connecté à la room  ${room}`)
-    })
-    socket.on('message', (msg) =>{
-      console.log('jjj'+ id_user)
-      io.to(room).emit('message', msg);
-      console.log('message: ' + msg);
-    })
-    socket.on('joinRoom',(data) =>{
-      socket.leave(room);
-      const clients1 = io.sockets.adapter.rooms.get(room);
-      console.log(clients1)
-      console.log(data)
-      room = data
-      socket.join(room)
-      const clients2 = io.sockets.adapter.rooms.get(room);
-      console.log(clients2)
-    })
+    console.log(`Connecté à la room  ${room}`)
+  })
+  socket.on('message', (msg) => {
+    console.log('jjj' + id_user)
+    io.to(room).emit('message', msg);
+    console.log('message: ' + msg);
+  })
+  socket.on('joinRoom', (data) => {
+    socket.leave(room);
+    const clients1 = io.sockets.adapter.rooms.get(room);
+    console.log(clients1)
+    console.log(data)
+    room = data
+    socket.join(room)
+    const clients2 = io.sockets.adapter.rooms.get(room);
+    console.log(clients2)
+  })
   //socket.emit('id',socket.id);
   // socket.on('message', (msg) =>{
   //   console.log('jjj'+ id_user)
@@ -75,18 +75,18 @@ io.on('connection',  (socket) =>{
   // console.log(clients)
   // console.log(clients2)
 
-//Limiter les rooms en nombres d'utilisateurs
+  //Limiter les rooms en nombres d'utilisateurs
 
-// if (clients === 1){
-//   socket.join(room);
-// } else if (clients === 2) {
-//   socket.join(room);
-// } else { // max two clients
-//   console.log("tes la")
-//   socket.disconnect()
-//   console.log(clients2)
-// }
-//   console.log(`Connecté à la room  ${room}`)
+  // if (clients === 1){
+  //   socket.join(room);
+  // } else if (clients === 2) {
+  //   socket.join(room);
+  // } else { // max two clients
+  //   console.log("tes la")
+  //   socket.disconnect()
+  //   console.log(clients2)
+  // }
+  //   console.log(`Connecté à la room  ${room}`)
 
 })
 
