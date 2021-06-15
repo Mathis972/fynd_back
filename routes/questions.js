d9670d0e8cc3b175fb8adbcc1d3690ffd63be70d
Mathis972 <eyzex@hotmail.com>
2021-06-15 11:44:05 +0200

    ajout methodes crud reponses/questions quizz

--- routes/questions.js
+++ routes/questions.js
@@ -1,5 +1,5 @@
-const express=require('express')
-const router=express.Router()
+const express = require('express')
+const router = express.Router()
 const { PrismaClient } = require('@prisma/client')
 const prisma = new PrismaClient()
 
@@ -7,5 +7,44 @@ router.get('/', async (req, res) => {
   const questions = await prisma.questions.findMany()
   res.json(questions)
 })
+router.post('/', async (req, res) => {
+  const question = await prisma.questions.create({
+    data: {
+      message: req.body.message
+    }
+  })
+  res.json(question)
+})
 
-module.exports=router
+router.put('/:id', async (req, res) => {
+  const { id } = req.params
+
+  const question = await prisma.questions.update({
+    where: {
+      id: parseInt(id)
+    },
+    data: {
+      message: req.body.message
+    }
+  })
+  res.json(question)
+})
+
+router.delete('/:id', async (req, res) => {
+  const { id } = req.params
+
+  await prisma.reponses.deleteMany({
+    where: {
+      fk_question_id: parseInt(id)
+    }
+  })
+
+  const question = await prisma.questions.delete({
+    where: {
+      id: parseInt(id)
+    }
+  })
+  res.json(`question : ${question.id} supprimée`)
+})
+
+module.exports = router
--- routes/reponses.js
+++ routes/reponses.js
@@ -1,5 +1,5 @@
-const express=require('express')
-const router=express.Router()
+const express = require('express')
+const router = express.Router()
 const { PrismaClient } = require('@prisma/client')
 const prisma = new PrismaClient()
 
@@ -8,4 +8,40 @@ router.get('/', async (req, res) => {
   res.json(reponses)
 })
 
-module.exports=router
+router.post('/', async (req, res) => {
+  const response = await prisma.reponses.create({
+    data: {
+      icon: req.body.icon,
+      fk_question_id: req.body.fk_question_id,
+      message_reponse: req.body.message_reponse
+    }
+  })
+  res.json(response)
+})
+
+router.put('/:id', async (req, res) => {
+  const { id } = req.params
+
+  const response = await prisma.reponses.update({
+    where: {
+      id: parseInt(id)
+    },
+    data: {
+      message_reponse: req.body.message_reponse
+    }
+  })
+  res.json(response)
+})
+
+router.delete('/:id', async (req, res) => {
+  const { id } = req.params
+
+  const response = await prisma.reponses.delete({
+    where: {
+      id: parseInt(id)
+    }
+  })
+  res.json(`réponse ${response.id} supprimée`)
+})
+
+module.exports = router
