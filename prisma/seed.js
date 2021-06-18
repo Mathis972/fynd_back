ce4b2b838b61b6657b4c9b5b45cbcf5901943389
Mathis972 <eyzex@hotmail.com>
2021-06-18 15:08:40 +0200

    ajout seed questions manquantes

--- prisma/seed.js
+++ prisma/seed.js
@@ -4,61 +4,124 @@ faker.locale = "fr";
 const prisma = new PrismaClient()
 
 const main = async () => {
-    await prisma.raisons_inscriptions.create({
-      data : {
-        libelle : 'selon feeling'
-      }
-    })
-    await prisma.raisons_inscriptions.create({
-      data : {
-        libelle : 'amitié'
-      }
-    })
-    await prisma.raisons_inscriptions.create({
-      data : {
-        libelle : 'soirée sans lendemain'
-      }
-    })
+  await prisma.raisons_inscriptions.create({
+    data: {
+      libelle: 'selon feeling'
+    }
+  })
+  await prisma.raisons_inscriptions.create({
+    data: {
+      libelle: 'amitié'
+    }
+  })
+  await prisma.raisons_inscriptions.create({
+    data: {
+      libelle: 'soirée sans lendemain'
+    }
+  })
 
   await prisma.questions.create({
     data: {
-      message : 'Quel hobby te correspond le mieux ?',
-      reponse : {
-        create : [
-        {
-          message_reponse : 'Voyage'
-        },
-        {
-          message_reponse : 'Sport'
-        },
-        {
-          message_reponse : 'Bien-être'
-        },
-        {
-          message_reponse : 'Films'
-        }
-      ]
+      message: 'Quel hobby te correspond le mieux ?',
+      reponse: {
+        create: [
+          {
+            message_reponse: 'Voyage'
+          },
+          {
+            message_reponse: 'Sport'
+          },
+          {
+            message_reponse: 'Bien-être'
+          },
+          {
+            message_reponse: 'Films'
+          }
+        ]
       }
     }
   })
   await prisma.questions.create({
     data: {
-      message : 'Tu gagnes 10 000 euros, tu ...',
-      reponse : {
-        create : [
-        {
-          message_reponse : 'Reverse tout à une association'
-        },
-        {
-          message_reponse : 'Offre un cadeau à tes parents ou à tes proches'
-        },
-        {
-          message_reponse : 'Pars en voyage'
-        },
-        {
-          message_reponse : 'Dépense tout rapidement'
-        }
-      ]
+      message: 'Tu gagnes 10 000 euros, tu ...',
+      reponse: {
+        create: [
+          {
+            message_reponse: 'Reverse tout à une association'
+          },
+          {
+            message_reponse: 'Offre un cadeau à tes parents ou à tes proches'
+          },
+          {
+            message_reponse: 'Pars en voyage'
+          },
+          {
+            message_reponse: 'Dépense tout rapidement'
+          }
+        ]
+      }
+    }
+  })
+  await prisma.questions.create({
+    data: {
+      message: 'Quel type de relation recherchez-vous ?',
+      reponse: {
+        create: [
+          {
+            message_reponse: 'Amicale'
+          },
+          {
+            message_reponse: 'Amoureuse'
+          },
+          {
+            message_reponse: 'En découverte'
+          },
+          {
+            message_reponse: 'Je ne sais pas'
+          }
+        ]
+      }
+    }
+  })
+  await prisma.questions.create({
+    data: {
+      message: 'Parmi ces animaux lequel préférez-vous ?',
+      reponse: {
+        create: [
+          {
+            message_reponse: 'Chat'
+          },
+          {
+            message_reponse: 'Chien'
+          },
+          {
+            message_reponse: 'Lapin'
+          },
+          {
+            message_reponse: 'Cheval'
+          }
+        ]
+      }
+    }
+  })
+  await prisma.questions.create({
+    data: {
+      message: 'Un petit déjeuner phare ?',
+      reponse: {
+        create: [
+          {
+            message_reponse: 'Croissant - Pain au chocolat'
+          },
+          {
+            message_reponse: 'Oeuf Bacon'
+          },
+          {
+            message_reponse: 'Céréales'
+          },
+          {
+            message_reponse: 'Pas de petit déjeuner'
+          }
+        ]
       }
     }
   })
@@ -67,32 +130,32 @@ const main = async () => {
   for (let index = 0; index <= 5; index++) {
     await prisma.utilisateurs.create({
       data: {
-        prenom : faker.name.firstName(),
-        email : faker.internet.email(),
-        est_admin : false,
-        mot_de_passe : faker.internet.password(),
-        date_de_naissance :faker.date.between('1997-01-01', '2015-01-05'),
-        biographie : faker.lorem.text(),
-        lri:{
-          create : {
-            raison_inscription :{
-              connect : {
-                id:Math.floor(Math.random() * 3)+1
+        prenom: faker.name.firstName(),
+        email: faker.internet.email(),
+        est_admin: false,
+        mot_de_passe: faker.internet.password(),
+        date_de_naissance: faker.date.between('1997-01-01', '2015-01-05'),
+        biographie: faker.lorem.text(),
+        lri: {
+          create: {
+            raison_inscription: {
+              connect: {
+                id: Math.floor(Math.random() * 3) + 1
               }
             }
           }
         },
-        photo_utilisateur : {
-          create : [
-          {
-          photo_url : faker.image.imageUrl(),
-          est_photo_profil : true,
-          },
-          {
-          photo_url : faker.image.imageUrl(),
-          est_photo_profil : true,
-          }
-        ]
+        photo_utilisateur: {
+          create: [
+            {
+              photo_url: faker.image.imageUrl(),
+              est_photo_profil: true,
+            },
+            {
+              photo_url: faker.image.imageUrl(),
+              est_photo_profil: true,
+            }
+          ]
         }
       }
     })
@@ -100,34 +163,34 @@ const main = async () => {
   //ajouter une conversation par rapport au utilisateur créer
   for (let index = 0; index <= 3; index++) {
     await prisma.conversations.create({
-      data : {
-        est_enregistre : true,
-        ajout_utilisateurs1:true,
-        ajout_utilisateurs2:true,
-        utilisateurs1 :{
-      connect : {
-        id : Math.floor(Math.random() * 5)+1
-      }
-      },
-      utilisateur2 : {
-        connect :{
-        id: Math.floor(Math.random() * 5) + 1
-      }
-      }
+      data: {
+        est_enregistre: true,
+        ajout_utilisateurs1: true,
+        ajout_utilisateurs2: true,
+        utilisateurs1: {
+          connect: {
+            id: Math.floor(Math.random() * 5) + 1
+          }
+        },
+        utilisateur2: {
+          connect: {
+            id: Math.floor(Math.random() * 5) + 1
+          }
+        }
       }
     })
   }
   //creation messages selon conversations
   for (let index = 0; index <= 10; index++) {
     await prisma.messages.create({
-      data : {
-        conversation : {
-          connect : {
-            id : Math.floor(Math.random() * 3) + 1
+      data: {
+        conversation: {
+          connect: {
+            id: Math.floor(Math.random() * 3) + 1
           }
         },
-        contenu : faker.lorem.paragraph(),
-        send_by_user1 :  Boolean(Math.round(Math.random()))
+        contenu: faker.lorem.paragraph(),
+        send_by_user1: Boolean(Math.round(Math.random()))
 
       }
     })
